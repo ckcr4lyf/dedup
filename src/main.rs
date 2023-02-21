bf2c54024237d615b4442d922854d020707a3814
ckcr4lyf <poiasdpoiasd@live.com>
2023-02-22 01:09:49 +0800

    Handle open error

--- src/main.rs
+++ src/main.rs
@@ -13,24 +13,25 @@ fn main() {
 
     for path in paths {
         if let Ok(p) = path {
-            let file = std::fs::File::open(p.path()).unwrap();
-            let mut bufreader = std::io::BufReader::new(&file);
-            let exifreader = exif::Reader::new();
-            
-            if let Ok(exif) = exifreader.read_from_container(&mut bufreader) {
-                for f in exif.fields() {
-                    match f.tag {
-                        exif::Tag::DateTime => {
-                            println!("{:?} - Found datetime as {}", p.file_name(), f.display_value())
-                        },
-                        _ => ()
+            if let Ok(file) = std::fs::File::open(p.path()) {
+                let mut bufreader = std::io::BufReader::new(&file);
+                let exifreader = exif::Reader::new();
+                
+                if let Ok(exif) = exifreader.read_from_container(&mut bufreader) {
+                    for f in exif.fields() {
+                        match f.tag {
+                            exif::Tag::DateTime => {
+                                println!("{:?} - Found datetime as {}", p.file_name(), f.display_value())
+                            },
+                            _ => ()
+                        }
                     }
-                    // println!("{} xxx {} xxx {}",
-                    //          f.tag, f.ifd_num, f.display_value().with_unit(&exif));
+                } else {
+                    println!("{:?} - EXIF data not found", p.file_name());
+                    // TODO: Some kinda fallback, e.g. filename rules?
                 }
             } else {
-                println!("{:?} - EXIF data not found", p.file_name());
-                // TODO: Some kinda fallback, e.g. filename rules?
+                println!("Failed to open {:?}", p.file_name());
             }
         }
     }
