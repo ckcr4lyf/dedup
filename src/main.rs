13887d2202c86eae523fe4af33bad0a0bd151577
ckcr4lyf <poiasdpoiasd@live.com>
2023-02-22 01:02:57 +0800

    some todos

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 fn main() {
-
     // TODO: Take in folder path as arg
     let paths = std::fs::read_dir("./ep/").unwrap();
 
     for path in paths {
         if let Ok(p) = path {
-            println!("Gonna do {}", p.path().display());
             let file = std::fs::File::open(p.path()).unwrap();
             let mut bufreader = std::io::BufReader::new(&file);
             let exifreader = exif::Reader::new();
@@ -14,7 +12,7 @@ fn main() {
                 for f in exif.fields() {
                     match f.tag {
                         exif::Tag::DateTime => {
-                            println!("Found datetime as {}", f.display_value())
+                            println!("{:?} - Found datetime as {}", p.file_name(), f.display_value())
                         },
                         _ => ()
                     }
@@ -22,7 +20,8 @@ fn main() {
                     //          f.tag, f.ifd_num, f.display_value().with_unit(&exif));
                 }
             } else {
-                println!("EXIF data not found for {}", p.path().display());
+                println!("{:?} - EXIF data not found", p.file_name());
+                // TODO: Some kinda fallback, e.g. filename rules?
             }
         }
     }
